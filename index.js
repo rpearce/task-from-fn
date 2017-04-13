a4c9aacabcaa8422901373dd85ac9b840c54ddbc
Robert Pearce <me@robertwpearce.com>
2017-04-13 22:15:30 +1200

    Update for es5 and move data.task to dependency

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+'use strict'
+
+const Task = require('data.task')
+
 function taskFromFn(fn, ctx) {
   return function() {
     var args = [].slice.call(arguments)
 
-    return new Task((reject, resolve) => {
+    return new Task(function(reject, resolve) {
       fn.apply(ctx,
-        args.concat(
-          (err, data) => err ? reject(err) : resolve(data)
-        )
+        args.concat(function(err, data) {
+          err ? reject(err) : resolve(data)
+        })
       )
     })
   }
